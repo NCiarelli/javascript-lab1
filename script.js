812dd41cd8dca6b3fec6ec00208dea4ad7186b0f
Ncia89 <nicholas.ciarelli@gmail.com>
2019-10-21 16:32:09 -0400

    Finished code for GC location if/else statement.

---
+++ script.js
@@ -0,0 +1,28 @@
+{
+  // Javascript Lab 1 SCript file
+
+  // Input Variable Declarations
+  let name = "Nicholas Ciarelli";
+  let age = 30;
+  let birthday = "April 9";
+  let detroitGC = true;
+  let lifeEvents = [
+    "I was born in Dearborn, Michigan.",
+    "I went to Wayne State University.",
+    " I placed 3rd in Disease Detective for Science Olympiad for all 4 yeears of high school.",
+    'I "ran" in the Tough Mudder in 2013.'
+  ];
+
+  // GC location if/else statement
+  if (detroitGC) {
+    console.log(
+      `My name is ${name} and I am a student at Grand Circus in Detroit, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`
+    );
+  } else {
+    console.log(
+      `My name is ${name} and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`
+    );
+  }
+
+  // Print out life events for loop
+}
