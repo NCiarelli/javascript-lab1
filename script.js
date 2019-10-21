26a8ff99934138c0b4f7499d2e4acfef0ec64452
Ncia89 <nicholas.ciarelli@gmail.com>
2019-10-21 17:03:23 -0400

    Added the random number while loop and corrected some typos.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
   let lifeEvents = [
     "I was born in Dearborn, Michigan.",
     "I went to Wayne State University.",
-    "I placed 3rd in Disease Detective for Science Olympiad for all 4 yeears of high school.",
+    "I placed 3rd in Disease Detective for Science Olympiad for all 4 years of high school.",
     'I "ran" in the Tough Mudder in 2013.'
   ];
 
@@ -27,5 +27,23 @@
   // Print out life events for loop
   for (i = 0; i < lifeEvents.length; i++) {
     console.log(lifeEvents[i]);
+  }
+
+  // Random number while loop
+  let counter = 0;
+  while (true) {
+    // Generate a random number from 1 to 10
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    // Check if it is not equal to 5 and print out the corresponding statement
+    if (randomNumber !== 5) {
+      counter++;
+      console.log(`${randomNumber} !== 5`);
+    } else {
+      counter++;
+      console.log(
+        `5 === 5. It took ${counter} iterations to randomly generate the number 5.`
+      );
+      break;
+    }
   }
 }
